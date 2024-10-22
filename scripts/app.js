22b081c8c4554d73b4ffa26d521c98caa91fed66
Saman <samanezzatabadi@yahoo.com>
2024-10-22 20:32:07 +0330

    2 sections done

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper.main", {
   speed: 500,
   slidesPerView: 1,
   spaceBetween: 10,
@@ -15,14 +15,59 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-const customPrevBtn = document.getElementById("homeslidernext");
-const customNextBtn = document.getElementById("homesliderprev");
+const Carswiper = new Swiper(".swiper.car", {
+  speed: 500,
+  slidesPerView: 1,
+  spaceBetween: 10,
 
-// Add event listeners to custom buttons
-customPrevBtn.addEventListener("click", function () {
-  swiper.slidePrev(); // Go to the previous slide
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2, // Show 1 slide per view
+      spaceBetween: 30,
+    },
+    1000: {
+      slidesPerView: 3, // Show 1 slide per view
+      spaceBetween: 30,
+    },
+  },
+
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
-customNextBtn.addEventListener("click", function () {
-  swiper.slideNext(); // Go to the next slide
+
+
+// const customPrevBtn = document.getElementById("homeslidernext");
+// const customNextBtn = document.getElementById("homesliderprev");
+
+// customPrevBtn.addEventListener("click", function () {
+//   swiper.slidePrev(); 
+// });
+
+// customNextBtn.addEventListener("click", function () {
+//   swiper.slideNext(); 
+// });
+
+
+const customPrevBtn2 = document.getElementById("carsSliderPrev");
+const customNextBtn2 = document.getElementById("carsSliderNext");
+
+customPrevBtn2.addEventListener("click", function () {
+  Carswiper.slidePrev(); 
+});
+
+customNextBtn2.addEventListener("click", function () {
+  Carswiper.slideNext();
 });
